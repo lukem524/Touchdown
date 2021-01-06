f23aae47a38c6996ca44a459a36e50ee79fdc92b
lukem524 <74715986+lukem524@users.noreply.github.com>
2021-01-06 18:50:45 +0100

    Scrolling Background
    - Created Quad 3D object to place as background.
    - Created a basic background from an image from google in photoshop, placed it as albido on a material.
    - Created script and code, background is currently scrolling.
    - Created Cube as player

---
+++ Touchdown/Assets/Scripts/scrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrolling : MonoBehaviour
+{
+
+    [SerializeField]
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x += Time.deltaTime/speed;
+        mat.mainTextureOffset = offset;
+    }
+}
