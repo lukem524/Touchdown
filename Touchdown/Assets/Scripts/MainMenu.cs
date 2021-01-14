41d0de4075527c14221f4b694027651588012078
AndyAttard <andy.attard.d55173@mcast.edu.mt>
2021-01-14 22:59:10 +0100

    Home Menu
    Created a Home Menu Selection with functioning buttons that include:
    
    -Play Button
    -Controls Button
    -Quit Button
    
    A back button was also included. After selecting controls, user will be able to return to the main menu selection by slection this button.

---
+++ Touchdown/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Calling the use for SceneManagement to have the ability to switch scenes
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        //Used SceneManger to load the current scene 'MainMenu' and added te value +1 to go to the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+        Debug.Log("Exiting Game");//Added Debug to make sure button is working since it is not possible to quit the application in inspector 
+    }
+}
