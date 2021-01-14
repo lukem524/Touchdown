8327fc9705978b7c539baf542eb55d912ad076e6
AndyAttard <andy.attard.d55173@mcast.edu.mt>
2021-01-14 23:20:41 +0100

    Returning to Main Menu
    Adjusted the script so that when the player dies, the game will return to the MainMenu Scene

--- Touchdown/Assets/Scripts/Damage.cs
+++ Touchdown/Assets/Scripts/Damage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Damage : MonoBehaviour
 {
@@ -22,6 +23,7 @@ public class Damage : MonoBehaviour
             if(player != null)
             {
                 Destroy(other.gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
             }
             
         }
