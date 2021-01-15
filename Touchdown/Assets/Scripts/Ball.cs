6f1125e16cdd60461e8fe5015d56e69712e0a27e
lukem524 <74715986+lukem524@users.noreply.github.com>
2021-01-15 02:40:41 +0100

    Did Ball system
    Created ball object and script
    Spawned Ball
    Did ball movement
    Destroy ball on collision with player
    tweaked the player and enemy speed

---
+++ Touchdown/Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ballLaunch();
+        if(transform.position.x < -1.5f){
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void ballLaunch(){
+        transform.Translate(new Vector3(-1f,0f,0f) * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Touchdown/Assets/Scripts/SpawnManager.cs
+++ Touchdown/Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _spawnPos;
+    [SerializeField]
+    private Transform _spawnPos_two;
+
+    [SerializeField]
+    private Transform _spawnPos_three;
 
     [SerializeField]
     private GameObject _enemyContainer;
     [SerializeField]
     private GameObject _enemyPrefab;
 
+    [SerializeField]
+    private GameObject _ballContainer;
+
+    [SerializeField]
+    private GameObject _ballPrefab;
+
     public bool _stopSpawning = false;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnRoutineBall());
+        StartCoroutine(SpawnRoutineBall_two());
+        StartCoroutine(SpawnRoutineBall_three());
     }
 
     // Update is called once per frame
@@ -30,11 +46,39 @@ public class SpawnManager : MonoBehaviour
 
             GameObject newEnemy = Instantiate(_enemyPrefab, spawn, Quaternion.identity);
             newEnemy.transform.SetParent(_enemyContainer.transform);
-            yield return new WaitForSeconds(Random.Range(1f, 5f));
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
         }
     }
 
-    public void OnPlayerDeath(){
+    IEnumerator SpawnRoutineBall()
+    {
+        while (_stopSpawning == false){
+            GameObject ball = Instantiate(_ballPrefab, _spawnPos.position, Quaternion.identity);
+            ball.transform.SetParent(_ballContainer.transform);
+            yield return new WaitForSeconds(Random.Range(6f, 9f));
+        }
+    }
+
+    IEnumerator SpawnRoutineBall_two()
+    {
+        while (_stopSpawning == false){
+            GameObject ball = Instantiate(_ballPrefab, _spawnPos_two.position, Quaternion.identity);
+            ball.transform.SetParent(_ballContainer.transform);
+            yield return new WaitForSeconds(Random.Range(3f, 7f));
+        } 
+    }   
+
+    IEnumerator SpawnRoutineBall_three()
+    {
+        while (_stopSpawning == false){
+            GameObject ball = Instantiate(_ballPrefab, _spawnPos_three.position, Quaternion.identity);
+            ball.transform.SetParent(_ballContainer.transform);
+            yield return new WaitForSeconds(Random.Range(4f, 8f));
+        } 
+    }
+
+public void OnPlayerDeath(){
         _stopSpawning = true;
     }
+
 }
